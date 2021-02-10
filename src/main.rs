e00a58b58deb62636f1cae52aa776383e26f3717
mieubrisse <mieubrisse@gmail.com>
2021-02-10 15:26:27 -0600

    Support multiple files

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{env, fs::canonicalize, process::exit};
+use std::{env, fs::canonicalize, path::PathBuf, process::exit};
 use anyhow::{Context, Result};
 use log::error;
 extern crate pretty_env_logger;
@@ -7,29 +7,38 @@ fn main() {
     pretty_env_logger::init();
     let args: Vec<String> = env::args().collect();
 
-    if args.len() != 3 {
-        error!("Takes two args: input_proto_filepath output_dirpath");
+    if args.len() <= 1 {
+        error!("Usage: {} input_dirpath output_dirpath input_filepath1 input_filepath2...", &args[0]);
         exit(1);
     }
 
-    let result = generate_protos(&args[1], &args[2]);
+    let raw_input_dirpath = &args[1];
+    let raw_output_dirpath = &args[2];
+    let raw_input_filepaths = &args[3..];
+
+    let result = generate_protos(raw_input_dirpath, raw_output_dirpath, raw_input_filepaths);
     if result.is_err() {
         error!("An error occurred generating the bindings: {:#}", result.unwrap_err());
         exit(1);
     }
 }
 
-fn generate_protos(raw_input_proto_filepath: &str, raw_output_dirpath: &str) -> Result<()> {
-    let input_proto_filepath = canonicalize(raw_input_proto_filepath)
-        .context("Could not canonicalize input proto filepath")?;
+fn generate_protos(raw_input_dirpath: &str, raw_output_dirpath: &str, raw_input_proto_filepaths: &[String]) -> Result<()> {
+    let input_dirpath = canonicalize(raw_input_dirpath)
+        .context(format!("Could not canonicalize raw input dirpath '{}'", raw_input_dirpath))?;
     let output_dirpath = canonicalize(raw_output_dirpath)
-        .context("Could not canonicalize output dirpath")?;
-    let input_proto_dirpath = input_proto_filepath.join("..").canonicalize()
-        .context("Could not canonicalize input proto dirpath")?;
+        .context(format!("Could not canonicalize raw output dirpath '{}'", raw_output_dirpath))?;
+
+    let mut input_proto_filepaths: Vec<PathBuf> = Vec::new();
+    for raw_filepath in raw_input_proto_filepaths {
+        let can_filepath = canonicalize(raw_filepath)
+            .context(format!("Could not canonicalize raw proto filepath '{}'", raw_filepath))?;
+        input_proto_filepaths.push(can_filepath);
+    }
 
     tonic_build::configure()
         .out_dir(&output_dirpath)
-        .compile(&[input_proto_filepath], &[input_proto_dirpath])
+        .compile(&input_proto_filepaths, &[input_dirpath])
         .context("An error occurred generating the .proto files")?;
     return Ok(());
 }
